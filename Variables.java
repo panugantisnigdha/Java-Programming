88dff7f3a02c6903ffd5b1dc93e3ccc7f21c4776
Panuganti Snigdha <panugantisnigdha@gmail.com>
2025-07-31 20:06:16 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,10 @@
+class Variables{
+    public static void main(String[] args){
+        int a=10;
+        int b=5;
+        String s="Snigdha";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(s);
+    }
+}
