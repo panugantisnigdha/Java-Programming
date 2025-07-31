517820b4a6168eca773ec1425aea06158b1d6411
Panuganti Snigdha <panugantisnigdha@gmail.com>
2025-07-31 20:18:12 +0530

    Create DataT.java

---
+++ DataT.java
@@ -0,0 +1,18 @@
+class DataT{
+    public static void main(String[] args){
+        // byte b=8;
+        // System.out.println(b);
+        // char ch='a';
+        // System.out.println(ch);
+        // boolean var=false;
+        // float price=1.5;
+        // int num=9;
+        // //long is used for large integers
+        // //double is used for large decimals
+        // short n=240;
+        int a=10;
+        int b=15;
+        int sum=a+b;
+        System.out.println("Sum="+sum);
+    }
+}
