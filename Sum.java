65767c2034055452c66b600ae81912f6ebcf4720
Panuganti Snigdha <panugantisnigdha@gmail.com>
2025-07-31 20:44:21 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Sum{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of a:");
+        int a=sc.nextInt();
+        System.out.println("Enter the value of b:");
+        int b=sc.nextInt();
+        int sum=a+b;
+        System.out.println("Sum of a and b:"+sum);
+       
+
+    }
+}
