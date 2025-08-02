e0c3c18b85850b7effe07eb2240763550b9b6cea
Panuganti Snigdha <panugantisnigdha@gmail.com>
2025-08-02 09:51:43 +0530

    Create ifelse.java

---
+++ ifelse.java
@@ -0,0 +1,19 @@
+class ifelse{
+    public static void main(String[] args){
+        int age=21;
+        if(age>=18){
+            System.out.println("Adult");
+
+        }else{
+            System.out.println("Not an Adult");//if else
+        }
+        int a=10;
+        int b=5;
+        if(a>=b){
+            System.out.println("A is largest");
+        }else{
+            System.out.println("B is largest");
+        }
+        
+    }
+}
