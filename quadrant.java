bbab91684d4b501e73bcaea0580a14b8e7733174
Panuganti Snigdha <panugantisnigdha@gmail.com>
2025-08-03 11:50:14 +0530

    Create quadrant.java

---
+++ quadrant.java
@@ -0,0 +1,15 @@
+class quadrant{
+    public static void main(String[] args){
+        int x=10;
+        int y=9;
+        if(x>0 &&y>0){
+            System.out.println("quadrant 1");
+        }else if(x<0 &&y>0){
+            System.out.println("quadrant 2");
+        }else if(x<0 &&y<0){
+            System.out.println("quadrant 3");
+        }else{
+            System.out.println("quadrant 4");
+        }
+    }
+}
