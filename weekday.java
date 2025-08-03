0a4a78fb292c71ea0b17f621b19b3b76af5592f8
Panuganti Snigdha <panugantisnigdha@gmail.com>
2025-08-03 14:17:04 +0530

    Create weekday.java

---
+++ weekday.java
@@ -0,0 +1,23 @@
+class weekday{
+    public static void main(String[] args){
+      String str="Wednesday";
+        switch(str){
+            case"Monday":System.out.println("Weekday");
+                         break;
+            case"Tuesday":System.out.println("Weekday");
+                          break;
+            case"Wednesday":System.out.println("Weekday");
+                            break;
+            case"Thursday":System.out.println("Weekday");
+                           break;
+            case"Friday":System.out.println("Weekday");
+                          break;
+            case"Saturday":System.out.println("Weekend");
+                           break;
+            case"Sunday":System.out.println("Weekend");
+                          break;
+            default:System.out.println("Invalid Input");
+            
+        }
+    }
+} 
