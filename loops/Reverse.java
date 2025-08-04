7fa506392f45028908e8cf091018cfc1e769a1bf
Panuganti Snigdha <panugantisnigdha@gmail.com>
2025-08-04 14:11:27 +0530

    Create Reverse.java

---
+++ loops/Reverse.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Reverse{
+    public static void main(String[] args){
+      int ld;
+      int n=10899;
+      while(n>0){
+          ld=n%10;
+          System.out.print(ld);
+          n=n/10;
+        }
+        
+    }
+} 
+      
