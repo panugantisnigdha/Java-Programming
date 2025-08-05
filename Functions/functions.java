2c34cae9b4a328638cff8865452bca87fbccbca5
Panuganti Snigdha <panugantisnigdha@gmail.com>
2025-08-05 20:44:46 +0530

    Create functions.java

---
+++ Functions/functions.java
@@ -0,0 +1,8 @@
+class functions{
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+        }
+    public static void main(String[] args){
+       printHelloWorld();    
+    }
+}
