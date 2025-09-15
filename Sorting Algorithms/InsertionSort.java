8a1205abb6e5805dd2a06f95c5c50f2f9249f677
Panuganti Snigdha <panugantisnigdha@gmail.com>
2025-09-15 20:48:00 +0530

    Update InsertionSort.java

--- Sorting Algorithms/InsertionSort.java
+++ Sorting Algorithms/InsertionSort.java
@@ -3,7 +3,7 @@ class InsertionSort{
   public static void InsertionSort(int arr[]){
         for(int i=1;i<arr.length;i++){
             int curr=arr[i];
-            int prev=i-1;
+            int prev=i-1; 
             while(prev>=0&&arr[prev]>curr){
                 arr[prev+1]=arr[prev];
                 prev--;
